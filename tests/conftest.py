8ea4f965e0ce2e46a37a1cfd094bb8a29a05c1f2
Chandler Scott <cscott@aquaveo.com>
2024-03-01 14:46:45 -0700

    Make the test scaffolding pass lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
-import pytest
+"""General configuration for tests."""
 from pathlib import Path
+
+import pytest
 
 
 @pytest.fixture
 def example_path():
+    """Return the path to the example directory."""
     return Path(__file__).parent / "example"
