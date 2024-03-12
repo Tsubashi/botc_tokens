014ce2040858b44ed915a84e85e80f163e7bcda7
Chandler Scott <cscott@aquaveo.com>
2024-03-12 14:31:52 -0600

    Get token_component coverage back to 100%

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,27 @@
 """General configuration for tests."""
-from pathlib import Path
+# Standard Library
+import zipfile
 
+# Third Party
 import pytest
+
+# Application Specific
+from botc_tokens.helpers.token_components import TokenComponents
 
 # Let pytest know we would like them to handle asserts in our helper code
 pytest.register_assert_rewrite("testhelpers")
 
 
-@pytest.fixture
-def example_path():
-    """Return the path to the example directory."""
-    return Path(__file__).parent / "example"
+@pytest.fixture()
+def component_package(tmp_path):
+    """Create a token component package."""
+    """Test that the token components are dumped as expected."""
+    # Create the token components
+    token_components = TokenComponents()
+
+    # Dump the token components
+    dump_path = tmp_path / "dump"
+    token_components.dump(dump_path)
+    return dump_path
+
+
